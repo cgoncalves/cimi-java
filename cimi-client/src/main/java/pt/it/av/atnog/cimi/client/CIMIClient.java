bff189a78cc79d3f1fd9ee7bf9a96bf79fb63fdb
Carlos Gonçalves <cgoncalves@av.it.pt>
2012-10-11 11:15:28 +0100

    Added license (LGPLv3)

--- cimi-client/src/main/java/pt/it/av/atnog/cimi/client/CIMIClient.java
+++ cimi-client/src/main/java/pt/it/av/atnog/cimi/client/CIMIClient.java
@@ -1,3 +1,23 @@
+/*-
+ * Copyright (C) 2012 ATNoG, Instituto de Telecomunicacoes, Portugal. All rights reserved.
+ *
+ * This file is part of CIMI-Java.
+ *
+ * CIMI-Java is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CIMI-Java is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CIMI-Java. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package pt.it.av.atnog.cimi.client;
 
 import java.net.URL;
